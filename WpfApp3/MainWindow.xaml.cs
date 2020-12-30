0218e3693c5d7c0e635f7879d0ddec88bb380421
UnstoppableRabbit <74426525+UnstoppableRabbit@users.noreply.github.com>
2020-12-30 19:15:07 +0300

    patch 0.1

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
+using System.Windows.Media.Animation;
 
 namespace WpfApp3
 {
@@ -25,11 +26,21 @@ namespace WpfApp3
     {
         MediaPlayer oxxy1;
         MediaPlayer oxxy2;
+        MediaPlayer miss;
+        MediaPlayer miss1;
+        MediaPlayer gavno;
+
+        int k = 0;
         public MainWindow()
         {
             oxxy1 = new MediaPlayer();
             oxxy2 = new MediaPlayer();
-            
+
+            miss = new MediaPlayer();
+            miss1 = new MediaPlayer();
+
+            gavno = new MediaPlayer();
+
             InitializeComponent();
 
             qwe.Text = Properties.Settings.Default.count.ToString();
@@ -43,7 +54,8 @@ namespace WpfApp3
         {
             oxxy1.Open(new Uri(@"C:\Users\ASUS\Downloads\Oxxxymiron_-_Do_zimy_47829574.mp3", UriKind.Absolute));
             oxxy1.Play();
-            oxxy1.Volume = 0.01;
+            oxxy1.Volume = 0.02;
+            gavno.Volume = 0.1;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -56,12 +68,38 @@ namespace WpfApp3
             Properties.Settings.Default.Save();
             qwe.Text = Properties.Settings.Default.count.ToString();
 
-            switch (Properties.Settings.Default.count)
+            Random rnd = new Random();
+            int value = rnd.Next(1, 100);
+
+            if (value == 13)
             {
-                case 7376:
-                    qwerty.ImageSource = new BitmapImage(new Uri(@"C:\Users\ASUS\Downloads\ac0b4c5133be5e6d2fe7326c6c47ebd6.jpg", UriKind.Relative));
-                    break;
+                TranslateTransform gnoj = new TranslateTransform();
+                slava.RenderTransform = gnoj;
+                DoubleAnimation animX = new DoubleAnimation(0, 200, TimeSpan.FromSeconds(1));
+                gnoj.BeginAnimation(TranslateTransform.XProperty, animX);
             }
+            if (value == 50)
+            {
+                TranslateTransform jhon = new TranslateTransform();
+                jhony.RenderTransform = jhon;
+                DoubleAnimation animY = new DoubleAnimation(0, -200, TimeSpan.FromSeconds(1));
+                jhon.BeginAnimation(TranslateTransform.YProperty, animY);
+            }
+            if (value == 10)
+            {
+                TranslateTransform qwer = new TranslateTransform();
+                lsp.RenderTransform = qwer;
+                DoubleAnimation animX = new DoubleAnimation(0, -700, TimeSpan.FromSeconds(2));
+                qwer.BeginAnimation(TranslateTransform.XProperty, animX);
+                DoubleAnimation animY = new DoubleAnimation(0, -500, TimeSpan.FromSeconds(2));
+                qwer.BeginAnimation(TranslateTransform.YProperty, animY);
+            }
+            //switch (Properties.Settings.Default.count)
+            //{
+            //    case 7376:
+            //        qwerty.ImageSource = new BitmapImage(new Uri(@"C:\Users\ASUS\Downloads\ac0b4c5133be5e6d2fe7326c6c47ebd6.jpg", UriKind.Relative));
+            //        break;
+            //}
         }
 
         private void mute_button_Click(object sender, RoutedEventArgs e)
@@ -76,7 +114,7 @@ namespace WpfApp3
                 oxxy1.Volume = 0;
                 muteImage.Source = new BitmapImage(new Uri(@"C:\Users\ASUS\Downloads\mute.jpg", UriKind.RelativeOrAbsolute));
             }
-                
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -90,8 +128,78 @@ namespace WpfApp3
 
         private void Double_missclick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("qwe");
+            Random rnd = new Random();
+            int value = rnd.Next(1, 3);
+            if (value == 1)
+                miss.Open(new Uri(@"C:\Users\ASUS\Downloads\otnoshenie.mp3", UriKind.Absolute));
+            else
+                miss.Open(new Uri(@"C:\Users\ASUS\Downloads\avprochempohui.mp3", UriKind.Absolute));
+
+            miss.Play();
+
+        }
+        private void gnoynij_Click(object sender, RoutedEventArgs e)
+        {
+            TranslateTransform gnoj = new TranslateTransform();
+            slava.RenderTransform = gnoj;
+            DoubleAnimation animX = new DoubleAnimation(0, -200, TimeSpan.FromSeconds(1));
+            gnoj.BeginAnimation(TranslateTransform.XProperty, animX);
+
+            miss1.Open(new Uri(@"C:\Users\ASUS\Desktop\for_clicker\podgotovka.wma", UriKind.Absolute));
+            miss1.Play();
         }
 
+        private void jhony_Click(object sender, RoutedEventArgs e)
+        {
+            switch (k)
+            {
+                case 0:
+                    gavno.Open(new Uri(@"C:\Users\ASUS\Desktop\for_clicker\govno.wma", UriKind.Absolute));
+                    gavno.Play();
+                    break;
+                case 1:
+                    gavno.Open(new Uri(@"C:\Users\ASUS\Desktop\for_clicker\zalupa.wma", UriKind.Absolute));
+                    gavno.Play();
+                    break;
+                case 2:
+                    gavno.Open(new Uri(@"C:\Users\ASUS\Desktop\for_clicker\penis.wma", UriKind.Absolute));
+                    gavno.Play();
+                    break;
+                case 3:
+                    gavno.Open(new Uri(@"C:\Users\ASUS\Desktop\for_clicker\her.wma", UriKind.Absolute));
+                    gavno.Play();
+                    break;
+                case 4:
+                    gavno.Open(new Uri(@"C:\Users\ASUS\Desktop\for_clicker\davalka.wma", UriKind.Absolute));
+                    gavno.Play();
+                    break;
+                case 5:
+                    gavno.Open(new Uri(@"C:\Users\ASUS\Desktop\for_clicker\hui.wma", UriKind.Absolute));
+                    gavno.Play();
+                    break;
+                case 6:
+                    gavno.Open(new Uri(@"C:\Users\ASUS\Desktop\for_clicker\blyadina.wma", UriKind.Absolute));
+                    gavno.Play();
+
+                    TranslateTransform jhon = new TranslateTransform();
+                    jhony.RenderTransform = jhon;
+                    DoubleAnimation animY = new DoubleAnimation(0, 200, TimeSpan.FromSeconds(1));
+                    jhon.BeginAnimation(TranslateTransform.YProperty, animY);
+                    k = -1;
+                    break;
+            }
+            k++;
+        }
+        private void olegi_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                TranslateTransform qwer = new TranslateTransform();
+                lsp.RenderTransform = qwer;
+                DoubleAnimation animX = new DoubleAnimation(0, 700, TimeSpan.FromSeconds(2));
+                qwer.BeginAnimation(TranslateTransform.XProperty, animX);
+                DoubleAnimation animY = new DoubleAnimation(0, 500, TimeSpan.FromSeconds(2));
+                qwer.BeginAnimation(TranslateTransform.YProperty, animY);
+            }
+        }
     }
 }
